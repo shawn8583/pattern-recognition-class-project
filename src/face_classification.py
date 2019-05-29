f4c609d6c0cdde604a81b6d229d200fc27397253
shawn8583 <Shawn8583@163.com>
2019-05-29 10:49:22 +0800

    complete converting original data to csv format

--- src/face_classification.py
+++ src/face_classification.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import numpy as np 
 import matplotlib as mpl
 from matplotlib import pyplot as plt
@@ -8,18 +9,35 @@ from matplotlib import pyplot as plt
 # reading eigenface data
 training_data = '/home/shawn/projects/pattern_recognition_class_project/face/csv_eigenfaces/faceR'  
 training_data_csv = '/home/shawn/projects/pattern_recognition_class_project/face/csv_eigenfaces/faceR_csv'      # This is a path in my Ubuntu Linux
-
 testing_data = '/home/shawn/projects/pattern_recognition_class_project/face/csv_eigenfaces/faceS'
 
+rename = '/home/shawn/projects/pattern_recognition_class_project/face/csv_eigenfaces/faceR_csv'
+newname = '/home/shawn/projects/pattern_recognition_class_project/face/csv_eigenfaces/faceR_csv.csv'
+
+# create "faceR_csv.csv" file for csv format data
+os.mknod(training_data_csv)
+
 # converting the data to csv format
-# faceR = open(training_data, "a+")
 with open(training_data, 'r+') as f:
       lines = f.readlines()
-      for i in range(1, len(lines)):
-            lines[i] = lines[i].strip()
+      for i in range(0, len(lines)):
+            lines[i] = lines[i].lstrip()  # There are spaces as the first character in the oroginal data file, .lstrip will delete the spaces
+            lines[i] = lines[i].replace(' ', ',') # replacing "space" with "comma" makes the data in csv format
 
 with open(training_data_csv, 'r+') as training_csv:
       training_csv.writelines(lines)
-            
+
+# with open(training_data_csv, 'r+') as training_csv:   # write chages
+#       training_csv.writelines(lines)
+
+# with open(training_data_csv, 'r+') as training_csv:
+#       csv_data = training_csv.read()
+#       csv_data = csv_data.replace(' ', ',')
+#       training_csv.truncate(0)
+#       training_csv.write(csv_data)
+
+os.rename(rename, newname)
+
+
 # csv_faceR = txt_faceR.replace(' ', ',') # replace space by comma
 # faceR.close()
